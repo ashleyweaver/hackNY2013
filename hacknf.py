5eb00c41ae6301e9b016c3efe44a063d9492f14e
Dinesh Dommaraju <dineshdommaraju@172-30-28-241.dynapool.nyu.edu>
2013-09-29 06:14:27 -0400

    hacknf

---
+++ hacknf.py
@@ -0,0 +1,28 @@
+import requests
+import urllib2
+import pprint
+from geopy import geocoders
+import json
+g = geocoders.GoogleV3()
+pp = pprint.PrettyPrinter(indent=4)
+address = raw_input('Enter the address: ')
+place, (lat, lng) = g.geocode(address)
+print "%s: %.5f, %.5f" % (place, lat, lng)
+url = "https://maps.googleapis.com/maps/api/place/search/json?location=%s,%s&radius=10&sensor=false&key=AIzaSyBYRMJfTpRcV-KEPuyzidZDXhvptBse6us" %(lat,lng)
+'''Request'''
+response = requests.get(url)
+data = json.loads(response.text)
+file = open('alchemy.txt', 'w+')
+for test in data['results']:
+        list=test['name']
+        print(list)
+        list = "%22" + str(list) +"%22"
+        urll = "http://api.nytimes.com/svc/search/v1/article?format=json&query=%s&api-key=f7eeff97d545f21eef9fe0ecdf99b655:17:68190350"%         (list)
+        response1 = requests.get(urll)
+        data1 = json.loads(response1.text)
+        for urtest in data1['results']:
+                listurl=urtest['url']
+                response = urllib2.urlopen('http://python.org/')
+                for urlline in response.read():
+                        file.write(urlline)
+
